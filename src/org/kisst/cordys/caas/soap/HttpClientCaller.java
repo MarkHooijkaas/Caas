a977f9c262fb47787bbd0f65e187cdd04d032d21
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-09-29 19:53:19 +0200

    removed some old stuff

--- src/org/kisst/cordys/caas/soap/HttpClientCaller.java
+++ src/org/kisst/cordys/caas/soap/HttpClientCaller.java
@@ -15,9 +15,6 @@ import org.apache.commons.httpclient.methods.StringRequestEntity;
 
 public class HttpClientCaller implements SoapCaller {
 	private final HttpClient client = new HttpClient();
-	//private final String dn;
-	//private final String orgdn;
-	//private final String org;
 	private final String url;
 	private final String username;
 	private final String password;
@@ -41,22 +38,12 @@ public class HttpClientCaller implements SoapCaller {
 			}
 			catch (java.io.IOException e) { throw new RuntimeException(e);  }
 		}
-		//dn      =(String) properties.get("cordys.dn");
-		//org =(String) properties.get("cordys.organization");
-		//orgdn= "o="+org+","+dn;
 
-		String tmpurl      =(String) properties.get("cordys.gateway.url");
+		url =(String) properties.get("cordys.gateway.url");
 		username=(String) properties.get("cordys.gateway.username");
 		password=(String) properties.get("cordys.gateway.password");
 		ntlmhost=(String) properties.get("cordys.gateway.ntlmhost");
 		ntlmdomain=(String) properties.get("cordys.gateway.ntlmdomain");
-		//url="http://"+host+":"+port+"/cordys/com.eibus.web.soap.Gateway.wcp?organization="+organization;
-		if (tmpurl.indexOf("Gateway.wcp")<=0) {
-			if (! tmpurl.endsWith("/"))
-				tmpurl+="/";
-			//tmpurl+="com.eibus.web.soap.Gateway.wcp?organization="+orgdn;
-		}
-		url=tmpurl;
 		if (ntlmdomain==null)
 			client.getState().setCredentials(AuthScope.ANY,	new UsernamePasswordCredentials(username, password));
 		else
@@ -70,8 +57,6 @@ public class HttpClientCaller implements SoapCaller {
 		int statusCode;
 		String response;
 		try {
-			//HttpState state=new HttpState();
-			//state.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 			method.setRequestEntity(new StringRequestEntity(input, "text/xml", "UTF-8"));
 			statusCode = client.executeMethod(method);
 			response=method.getResponseBodyAsString();
