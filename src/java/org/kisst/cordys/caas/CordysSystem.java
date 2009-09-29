4cf1e31f4e6ab97c945332629b3ce66d5197e2f9
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-09-29 20:34:56 +0200

    Added getOrganizations method

--- src/java/org/kisst/cordys/caas/CordysSystem.java
+++ src/java/org/kisst/cordys/caas/CordysSystem.java
@@ -4,21 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.kisst.cordys.caas.soap.SoapCaller;
 
 
 public class CordysSystem extends CordysObject {
-	private final String orgdn;
+	private final String rootdn;
 	private final SoapCaller caller;
 
 	public CordysSystem(String dn, SoapCaller caller) {
 		super(dn);
 		this.caller=caller;
-		this.orgdn=dn;
+		this.rootdn=dn;
 	}
-	private String cordysCall(String input) {
-		return caller.call(input);
+	private String call(String input) {
+		String soap="<SOAP:Envelope xmlns:SOAP=\"http://schemas.xmlsoap.org/soap/envelope/\"><SOAP:Body>"
+			+ input
+			+ "</SOAP:Body></SOAP:Envelope>";
+		return caller.call(soap);
+	}
+	
+	public List<Organization> getOrganizations() {
+		String msg="<GetAllOrganizations xmlns=\"http://schemas.cordys.com/1.0/ldap\">"
+			+"<dn>${rootdn}</dn>"
+		    +"</GetAllOrganizations>";
+		msg = msg.replaceAll("\\$\\{rootdn}",rootdn);
+		String response=call(msg);
+		ArrayList<Organization> result=new ArrayList<Organization>();
+		int pos=0;
+		String key="entry dn=\"";
+		while ((pos=response.indexOf(key, pos))>0) {
+			pos=pos+key.length()+1;
+			String dn=response.substring(pos,response.indexOf("\"", pos));
+			result.add(new Organization(dn));
+			System.out.println(dn);
+		}
+		return result;
 	}
 
 	private String loadTemplate(String name) {
@@ -38,7 +61,7 @@ public class CordysSystem extends CordysObject {
 		template = template.replaceAll("\\$\\{dn}",dn);
 		template = template.replaceAll("\\$\\{name}",name);
 		template = template.replaceAll("\\$\\{org}",org);
-		System.out.println(cordysCall(template));
+		System.out.println(call(template));
 	}
 
 	public void createAuthUser(String login, String name, String fullname) {
@@ -47,7 +70,7 @@ public class CordysSystem extends CordysObject {
 		template = template.replaceAll("\\$\\{name}",name);
 		template = template.replaceAll("\\$\\{fullname}",fullname);
 		template = template.replaceAll("\\$\\{login}",login);
-		System.out.println(cordysCall(template));
+		System.out.println(call(template));
 	}
 
 	public void createOrgUser(String org, String authname, String name, String fullname, String role) {
@@ -58,36 +81,36 @@ public class CordysSystem extends CordysObject {
 		template = template.replaceAll("\\$\\{authname}",authname);
 		template = template.replaceAll("\\$\\{fullname}",fullname);
 		template = template.replaceAll("\\$\\{role}",role);
-		System.out.println(cordysCall(template));
+		System.out.println(call(template));
 	}
 
 	
 	public void createMethodSet(String conntype, String name, String namespace) {
 		String template=loadTemplate("CreateMethodSet.template");
-		template = template.replaceAll("\\$\\{org}",orgdn);
+		template = template.replaceAll("\\$\\{org}",rootdn);
 		template = template.replaceAll("\\$\\{name}",name);
 		template = template.replaceAll("\\$\\{namespace\\}",namespace);
 		template = template.replaceAll("\\$\\{type\\}",conntype);
-		System.out.println(cordysCall(template));
+		System.out.println(call(template));
 	}
 	public void deleteMethodSet(String conntype, String name, String namespace) {
 		String template=loadTemplate("DeleteMethodSet.template");
-		template = template.replaceAll("\\$\\{org}",orgdn);
+		template = template.replaceAll("\\$\\{org}",rootdn);
 		template = template.replaceAll("\\$\\{name}",name);
 		template = template.replaceAll("\\$\\{namespace\\}",namespace);
 		template = template.replaceAll("\\$\\{type\\}",conntype);
 		System.out.println(template);
-		System.out.println(cordysCall(template));
+		System.out.println(call(template));
 	}	
 	public void createMethod(String methodset, String name, String impl, String wsdl) {
 		String template=loadTemplate("CreateMethod.template");
-		template = template.replaceAll("\\$\\{org}",orgdn);
+		template = template.replaceAll("\\$\\{org}",rootdn);
 		template = template.replaceAll("\\$\\{methodset}",methodset);
 		template = template.replaceAll("\\$\\{name\\}",name);
 		template = template.replaceAll("\\$\\{impl\\}",xmlEscape(impl));
 		template = template.replaceAll("\\$\\{wsdl\\}",xmlEscape(wsdl));
 		System.out.println(template);
-		System.out.println(cordysCall(template));
+		System.out.println(call(template));
 	}
 	private String xmlEscape(String str) {
 		return str.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
---
+++ src/java/org/kisst/cordys/caas/Organization.java
@@ -0,0 +1,8 @@
+package org.kisst.cordys.caas;
+
+public class Organization extends CordysObject {
+	public Organization(String dn) {
+		super(dn);
+	}
+
+}
