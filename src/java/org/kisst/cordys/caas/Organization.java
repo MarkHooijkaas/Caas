7980d866109ed196871a67d14f9d8791d4e48ecc
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-10-01 17:01:09 +0200

    added user property to organization

--- src/java/org/kisst/cordys/caas/Organization.java
+++ src/java/org/kisst/cordys/caas/Organization.java
@@ -3,10 +3,14 @@ package org.kisst.cordys.caas;
 import java.util.List;
 
 import org.jdom.Element;
+import org.kisst.cordys.caas.util.DynamicProperty;
 
 public class Organization extends CordysObject {
+	public final DynamicProperty<User> user;
+
 	public Organization(CordysSystem system, String dn) {
 		super(system, dn);
+		user=new DynamicProperty<User>(getSystem(), User.class, "cn=", "cn=organizational users,"+dn);
 	}
 
 	public List<User> getUsers() {	
