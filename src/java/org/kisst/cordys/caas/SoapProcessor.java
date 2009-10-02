ea5a6843aa7a27ed6824b51d578d6181b53db9ad
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-10-02 14:23:24 +0200

    added start and stop command's

--- src/java/org/kisst/cordys/caas/SoapProcessor.java
+++ src/java/org/kisst/cordys/caas/SoapProcessor.java
@@ -1,9 +1,29 @@
 package org.kisst.cordys.caas;
 
+import org.jdom.Element;
+import org.jdom.Namespace;
+
 public class SoapProcessor extends CordysObject {
+
+	private static final Namespace nsmonitor=Namespace.getNamespace("http://schemas.cordys.com/1.0/monitor");
 
 	public SoapProcessor(CordysObject parent, String dn) {
 		super(parent, dn);
 	}
 
+	public void start() {
+		Element method=new Element("Start", nsmonitor);
+		method.addContent(new Element("dn").setText(dn));
+		call(method);
+	}
+	public void stop() {
+		Element method=new Element("Stop", nsmonitor);
+		method.addContent(new Element("dn").setText(dn));
+		call(method);
+	}
+	public void restart() {
+		Element method=new Element("Restart", nsmonitor);
+		method.addContent(new Element("dn").setText(dn));
+		call(method);
+	}
 }
