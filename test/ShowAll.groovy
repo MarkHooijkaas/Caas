6e76676d5295ba9fe8a4ca6eb47a15774ae0be8e
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-10-03 09:30:31 +0200

    cleanup of all helper files

---
+++ test/ShowAll.groovy
@@ -0,0 +1,30 @@
+cordys=org.kisst.cordys.caas.CordysSystem.connect("user.properties")
+//println cordys.children
+for (i in cordys.isvps) {
+  println "" + i +" ==> " + i.dn
+  for (ms in i.methodSets) {
+    println "    " +ms
+    for (m in ms.methods)
+      println "        " +m
+  }
+  for (r in i.roles)
+    println "    " +r
+} 
+for (o in cordys.organizations) { 
+  println "" + o + " ==> " + o.dn
+  for (u in o.users)
+    println "    " +u
+  for (ms in o.methodSets) {
+    println "    " +ms
+    for (m in ms.methods)
+      println "        " +m
+  }
+  for (r in o.roles)
+    println "    " +r
+  for (s in o.soapNodes) {
+    println "    " +s
+    for (p in s.soapProcessors)
+      println "        " +p
+  }
+}
+
