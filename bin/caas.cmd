662883415ac5bfb6df5d6096d9c268c4f8601e9e
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-12-15 19:38:38 +0100

    fixed bad windows batch file

--- bin/caas.cmd
+++ bin/caas.cmd
@@ -2,5 +2,5 @@
 @set JAVA_EXE=java
 @set JAVA_OPTS=-Xmx256m  
 
-%JAVA_EXE% %JAVA_OPTS% -jar %dp0~/../caas.jar %1 %2 %3 %4 %5 %6 %7 %9 %9 
+%JAVA_EXE% %JAVA_OPTS% -jar ../caas.jar %1 %2 %3 %4 %5 %6 %7 %8 %9 
 
